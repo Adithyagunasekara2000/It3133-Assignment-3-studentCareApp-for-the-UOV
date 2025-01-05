2d56c9ba02bf75345e560e86e762ab385fdc3a59
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2025-01-05 22:35:49 +0530

    Profile editing

--- StudentCareApp/components/Profile.js
+++ StudentCareApp/components/Profile.js
@@ -61,6 +61,7 @@ const styles = StyleSheet.create({
       profileName: {
         fontSize: 24,
         fontWeight: 'bold',
+        alignItems: 'center',
       },
       profilePicContainer: {
         alignItems: 'center',
